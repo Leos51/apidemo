aec8664d72425cc0396b87b18f56c415a96d3eb3
Leos <abraham.adouane@gmail.com>
2026-02-05 08:28:00 +0100

    essai test unitaire github action - 2

--- infra/mysql/dump/script.sql
+++ infra/mysql/dump/script.sql
@@ -1,15 +1,17 @@
+USE apidemo_test;
+
 -- Création de la table
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS `user` (
                                       id INT AUTO_INCREMENT PRIMARY KEY,
                                       firstname VARCHAR(255) NOT NULL,
     lastname VARCHAR(255) NOT NULL
-    ) ENGINE=InnoDB ;
+    ) ENGINE=InnoDB;
 
--- Données de test (au moins Adele pour que le test passe)
+-- Données de test
 INSERT INTO Person (firstname, lastname) VALUES
                                              ('Alice', 'THORNTON'),
                                              ('Lance', 'RIDDLE'),
                                              ('Brandon', 'HUTTINS'),
                                              ('Bob', 'MARTIN');
 
-SELECT 'Database initialized with test data!' as Status;
+SELECT 'Database initialized with test data!' AS Status;
