70fb38dc1036e5e8015cb9806f153d2609721e6e
Leos <abraham.adouane@gmail.com>
2026-02-11 15:58:21 +0100

    test

--- src/test/java/training/afpa/cda24060/apidemo/ApiDemoApplicationTests.java
+++ src/test/java/training/afpa/cda24060/apidemo/ApiDemoApplicationTests.java
@@ -1,11 +1,18 @@
 package training.afpa.cda24060.apidemo;
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class ApiDemoApplicationTests {
 
     @Test
+    @Description("Test de chargement du contexte Spring Boot")
+    @Severity(SeverityLevel.BLOCKER)
     void contextLoads() {
     }
 
